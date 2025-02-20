3d71d60e4978f2b9a8700b82b6b256339c4e58e1
LKS <81739844+badewen@users.noreply.github.com.>
2025-02-20 15:04:39 +0700

    asdasd

--- vote/app.py
+++ vote/app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="master.lks-redis.blparj.use1.cache.amazonaws.com", port=6379, db=0, socket_timeout=5)
+        g.redis = Redis(host="192.168.10.28", port=6379, db=0, socket_timeout=5)
     return g.redis
   
 @app.route("/", methods=['POST','GET'])
